57be11ef028a0778a60b63c3eca818e709dc6b58
Etka <etkaaksoyy16@gmail.com>
2026-05-02 11:52:49 +0300

    filter fix

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ const matchSearch =
     </button>
 
     <button
-      onClick={() => setSearch("writing")}
+      onClick={() => setSearch("yazı")}
       className="px-4 py-2 rounded-lg transition hover:scale-105 bg-white/10 hover:bg-white/20"
     >
       ✍️ Yazı Yaz
