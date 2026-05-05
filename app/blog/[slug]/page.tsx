db8f7308f3d7093922cb3ec7d383233ab43cefcc
Etka <etkaaksoyy16@gmail.com>
2026-05-05 22:47:15 +0300

    blog titles fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({ params }: any) {
 
   return (
     <div className="max-w-3xl mx-auto mt-10">
-  <h1 className="text-4xl md:text-5xl font-bold mb-6 bg-gradient-to-r from-purple-400 to-blue-500 text-transparent bg-clip-text">
+  <h1 className="text-4xl md:text-5xl font-bold mb-6 mt-4 leading-[1.2] bg-gradient-to-r from-purple-400 to-blue-500 text-transparent bg-clip-text">
     {blog.title}
   </h1>
 
